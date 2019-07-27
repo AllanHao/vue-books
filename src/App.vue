e1023aec66875ae4f4fdb057bc70198f19e00d28
zhanghao <zhanghao103@ky-tech.com.cn>
2019-07-27 14:04:09 +0800

    book

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-menu default-active="frontend"
+             mode="horizontal"
+             @select="handleSelect">
+      <el-menu-item index="frontend">前端</el-menu-item>
+      <el-menu-item index="backend">后端</el-menu-item>
+      <el-menu-item index="sql">数据库</el-menu-item>
+      <el-menu-item index="dataStructure">数据结构与算法</el-menu-item>
+    </el-menu>
+    <!-- <router-view /> -->
+    <ul>
+      <li :key="item.name"
+          v-for="item in book">
+        <a :href="item.url"
+           target="_blank">
+          {{item.name}}
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import books from '../static/book-urls'
+  export default {
+    name: 'App',
+    data () {
+      return {
+        book: books['frontend']
+      }
+    },
+    methods: {
+      handleSelect (key, keyPath) {
+        this.book = books[key]
+        // this.$router.push({ name: 'books', params: { type: key } })
+        console.log(key, keyPath)
+      }
+    },
+    mounted () {
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body,
+  html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
---
+++ static/book-urls.js
@@ -0,0 +1,14 @@
+export default {
+    'frontend': [
+        { 'name': 'html5应用开发实践指南.azw3', 'url': 'haha' }
+    ],
+    'backend': [
+        { 'name': '后台.azw3', 'url': 'haha' }
+    ],
+    'sql': [
+        { 'name': 'sql.azw3', 'url': 'haha' }
+    ],
+    'dataStructure': [
+        { 'name': '算法.azw3', 'url': 'haha' }
+    ]
+}
